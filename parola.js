1718a54dd260151b39fc1c4a01579e088f04a5d9
ivipetkova <137443080+ivipetkova@users.noreply.github.com>
2023-06-22 23:54:52 +0300

    Create parola.js

---
+++ parola.js
@@ -0,0 +1,9 @@
+function start(input){
+let parola = input[0];
+if (parola === "s3cr3t!P@ssw0rd"){
+    console.log("Welcome");
+}else{
+    console.log("Wrong password!");
+}
+}
+start(["s3cr3t!P@ssw0rd"])
